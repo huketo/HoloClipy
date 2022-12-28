86980272197173d0ae19e0bc1cbd40f5cff53963
huketo <huketo@daum.net>
2022-12-29 01:52:21 +0900

    add: upload first

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -33,8 +33,42 @@ const videos = [
 export const trending = (req, res) => {
   return res.render("home", { pageTitle: "Home", fakeUser, videos });
 };
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
-export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
-export const search = (req, res) => res.render("search");
-export const upload = (req, res) => res.render("upload");
-export const deleteVideo = (req, res) => res.render("delete_video");
+
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watchching ${video.title}`, video });
+};
+
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Editting ${video.title}`, video });
+};
+
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+
+  const newVideo = {
+    title: title,
+    rating: 0,
+    comments: 0,
+    createdAt: "0 minutes ago",
+    views: 1,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+
+  return res.redirect("/");
+};
